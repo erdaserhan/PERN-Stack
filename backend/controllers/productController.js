473c2cb855e569a6f00ea9a666d1e54f68159385
erdaserhan <erhan209@gmail.com>
2025-02-12 18:50:43 +0100

    getProduct function ok

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -13,6 +13,7 @@ export const getProducts = async (req, res) => {
         res.status(500).json({ success: false, message: "Internal Server Error" });
     }
 };
+
 export const createProduct = async (req, res) => {
     const {name,price,image} = req.body // in server.js we have "app.use(express.json());". That's how we can get the datas
 
@@ -22,15 +23,30 @@ export const createProduct = async (req, res) => {
 
     try {
         const newProduct = await sql`
-         INSERT INTO products (name,price,image) VALUES (${name},${price},${image})
-         RETURNING *
+            INSERT INTO products (name,price,image) VALUES (${name},${price},${image})
+            RETURNING *
         `
-        res.status(201).json({ success: true, data: newProduct[0] });
+        res.status(201).json({ success: true, data: newProduct[0] }); //We get the [0] because const newProduct return us an array
     } catch (error) {
         console.log("Error in createProduct function", error)
         res.status(500).json({ success: false, message: "Internal Server Error" });
     }
 };
-export const getProduct = async (req, res) => {};
+
+export const getProduct = async (req, res) => {
+    const { id } = req.params // We get the {id} because in router we called it id = router.get('/:id', getProduct);
+    try {
+        const product = await sql`
+            SELECT * FROM products WHERE id=${id}
+        `
+
+        res.status(200).json({ success: true, data: product[0] });
+    } catch (error) {
+        console.log("Error in getProduct function", error)
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+};
+
 export const updateProduct = async (req, res) => {};
+
 export const deleteProduct = async (req, res) => {};
