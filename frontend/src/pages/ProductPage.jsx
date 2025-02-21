37599848a09e64ee766ca35e8778770abe588c4f
erdaserhan <erhan209@gmail.com>
2025-02-21 12:15:35 +0100

    ProductPage error and loading states updated

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,4 +1,41 @@
+import { useNavigate, useParams } from "react-router-dom"
+import { useProductStore } from "../store/useProductStore"
+import { useEffect } from "react"
+
 function ProductPage() {
+  const { 
+    currentProduct,
+    formData,
+    setFormData,
+    loading,
+    error,
+    fetchProduct,
+    updateProduct,
+    deleteProduct
+   } = useProductStore()
+   const navigate = useNavigate()
+   const {id} = useParams()
+
+   useEffect(() => {
+      fetchProduct(id)
+    }, [fetchProduct, id])
+
+    if (loading) {
+      return (
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="loading loading-spinner loading-lg"/>
+        </div>
+      )
+    }
+
+    if (error) {  
+      return (
+        <div className="container mx-auto px-4 py-8">
+          <div className="alert alert-error">{error}</div>
+        </div>
+      );
+    }
+    
   return (
     <div>ProductPage</div>
   )
