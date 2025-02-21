7035d7704c465fd2bcf191aa24e2785a296aa47f
erdaserhan <erhan209@gmail.com>
2025-02-21 13:22:40 +0100

    port updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
 import { aj } from "./lib/arcjet.js";
@@ -11,10 +13,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet()); // helmet is a security middleware that helps us protect our app by setting various HTTP headers
+app.use(
+    helmet({
+        contentSecurityPolicy: false
+    })); // helmet is a security middleware that helps us protect our app by setting various HTTP headers
 app.use(morgan("dev")); //log the requests
 
 // apply arcjet rate-limit to all routes
@@ -49,6 +55,14 @@ app.use(async (req, res, next) => {
 });
 
 app.use('/api/products', productRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 async function initDB() {
     try {
